41d6bbbce955c87ebc1be1161494f550e4ca8f8f
Vanshika Johari <vanshikaj256@gmail.com>
2024-07-28 14:31:45 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(StopwatchApp());
+}
+
+class StopwatchApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stopwatch',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StopwatchHomePage(),
+    );
+  }
+}
+
+class StopwatchHomePage extends StatefulWidget {
+  @override
+  _StopwatchHomePageState createState() => _StopwatchHomePageState();
+}
+
+class _StopwatchHomePageState extends State<StopwatchHomePage> {
+  late Stopwatch _stopwatch;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _stopwatch = Stopwatch();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {});
+    });
+  }
+
+  void _startStopwatch() {
+    _stopwatch.start();
+    _startTimer();
+  }
+
+  void _stopStopwatch() {
+    _stopwatch.stop();
+    _timer.cancel();
+  }
+
+  void _resetStopwatch() {
+    _stopwatch.reset();
+    setState(() {});
+  }
+
+  String _formatTime(int milliseconds) {
+    int hundredths = (milliseconds / 10).truncate();
+    int seconds = (hundredths / 100).truncate();
+    int minutes = (seconds / 60).truncate();
+
+    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
+    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
+    String hundredthsStr = (hundredths % 100).toString().padLeft(2, '0');
+
+    return "$minutesStr:$secondsStr:$hundredthsStr";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stopwatch'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _formatTime(_stopwatch.elapsedMilliseconds),
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _stopwatch.isRunning ? _stopStopwatch : _startStopwatch,
+                  child: Text(_stopwatch.isRunning ? 'Pause' : 'Start'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _resetStopwatch,
+                  child: Text('Reset'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
